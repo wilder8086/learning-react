e632a24379d8f72aa430d352c4a81481006aabce
VSIWIDEL <EODrigo2013LIZ>
2021-09-29 23:27:28 -0300

    SE updatea HeaderComponent para agregar referencias a inputs del Dom para despues acceder a sus valores

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody, FormGroup, Label, Input, Form } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
 
-    this.toggleNav = this.toggleNav.bind(this);
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      isModalOpen: false
     };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   toggleNav() {
     this.setState({
       isNavOpen: !this.state.isNavOpen
     });
+  }
+
+  toggleModal(){
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });    
+  }
+
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username : " + this.username.value + " Password : " + this.password.value
+      + " Remember : " + this.remember.checked);
+    
+      event.preventDefault();
   }
 
   render() {
@@ -50,6 +67,13 @@ class Header extends React.Component {
                   </NavLink>
                 </NavItem>
               </Nav>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span> Login
+                  </Button>
+                </NavItem>
+              </Nav>
             </Collapse>
           </div>
         </Navbar>
@@ -64,6 +88,34 @@ class Header extends React.Component {
             </div>
           </div>
         </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            {/* vamos a configurar el formulario no controlado dentro de ModalBody */}
+            {/* como recuperamos los valores del fomrulario ? necesitamos usar ref al formulario (referencias al formulario) de cada entrada. Los componenetes de reactstrap ya usan ref, entonces usamos innerRef en lugar de ref.
+            Con innerRef estamos extrayendo la Referencia de los inputs del DOM donde guardan los valores y luego ponerlos a disposicion de react a traves de la funcion handleLogin donde extraigo los valores como this.username.value, this.password.value y this.remeber.checked y luego poder ver esos valores */}
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" name="username" id="username"
+                  innerRef={(input) => this.username = input}/>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="text" name="password" id="password"
+                innerRef={(input) => this.password = input}/>
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input type="checkbox" name="remember"
+                  innerRef={(input) => this.remember = input}/>  
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">Login</Button>
+            </Form>
+          </ModalBody>
+        </Modal>
       </>
     );
   }
