e14ccf392766e6d748cd927dc84b23f241e19268
VSIWIDEL <EODrigo2013LIZ>
2021-10-11 21:55:03 -0300

    Se agrega combineReducers para desacoplar nuestro reducer en reducers mas simples para luego combnarlo/componerlo

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,20 @@
-import { createStore } from 'redux';
-import { Reducer, initialState } from './reducer';
+import { createStore, combineReducers } from 'redux';
+// funciones reductoras mas simples
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Leaders } from './leaders';
+import { Promotions } from './promotions';
 
 // cuando alguien invoca a este configureStore configura mi redux store en la app
 export const ConfigureStore = () => {
   const store = createStore(
-    Reducer,
-    initialState
+    // combinamos nuestras funciones reductoras simples a un estado mas general mediante combineReducers
+    combineReducers({ 
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders
+    })
   );
 
   return store;
