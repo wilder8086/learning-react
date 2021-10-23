95b5f72057d7a19012c71e51dc34352cfa470f21
VSIWIDEL <EODrigo2013LIZ>
2021-10-23 14:11:55 -0300

    Se agrega React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import Loading from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
 const required = (val) => val && val.length
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,13 +16,19 @@ function RenderDish({ dish }) {
   if (dish != null) {
     return (
       <div className='col-12 col-md-5 m-1'>
-        <Card>
-          <CardImg width="100%" src={ baseUrl + dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+          }}>
+          <Card>
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </FadeTransform>
       </div>
     )
   } else {
@@ -40,17 +48,21 @@ function RenderComments({ comments, postComment, dishId }) {
     const commentsLi = comments.map((c) => {
       console.log("uno : ", c);
       return (
-        <li key={c.id}>
-          <p>{c.comment}</p>
-          <p>--{c.author}, {formattedDate(c.date)}</p>
-        </li>
+        <Fade in>
+          <li key={c.id}>
+            <p>{c.comment}</p>
+            <p>--{c.author}, {formattedDate(c.date)}</p>
+          </li>
+        </Fade>
       );
     });
     return (
       <div className='col-12 col-md-5 m-1'>
         <h4> Comments </h4>
         <ul className='list-unstyled'>
-          {commentsLi}
+          <Stagger in>
+            {commentsLi}
+          </Stagger>
         </ul>
         <CommentForm dishId={dishId} postComment={postComment} />
       </div>
@@ -179,9 +191,9 @@ const Dishdetail = (props) => {
             </div>
           </div>
         )
-      }  
+      }
     }
-  }    
+  }
 }
 
 
